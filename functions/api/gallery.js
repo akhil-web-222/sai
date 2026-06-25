51408b47442fed9815e24c17dfb34102de115da2
A-Akhil <akhilrahul70@gmail.com>
2026-06-25 12:55:05 +0530

    feat: add house work album mapping to gallery configuration

--- functions/api/gallery.js
+++ functions/api/gallery.js
@@ -3,7 +3,8 @@ const FOLDER_CATEGORY_MAP = {
   'sai-photo/album/customized-work': 'filter-product',
   'sai-photo/album/construction-work': 'filter-branding',
   'sai-photo/album/hotel-apartments': 'filter-books',
-  'sai-photo/album/mansion-builders': 'filter-mansion'
+  'sai-photo/album/mansion-builders': 'filter-mansion',
+  'sai-photo/album/house work - 1': 'filter-house-work-1'
 };
 
 async function fetchCloudinaryResources(cloudName, apiKey, apiSecret, prefix) {
